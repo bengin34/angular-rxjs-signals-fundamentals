4d37fe510e54714a74453afe981e4d57fd12aad6
bengin34 <becaglar3434@gmail.com>
2024-08-18 14:43:04 +0200

    retrieving data

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,10 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from './product';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  // Just enough here for the code to compile
   private productsUrl = 'api/products';
 
+  // constructor(private http: HttpClient){} or the other way is using inject()
+  private http = inject(HttpClient);
+
+  getProducts(): Observable<Product[]> {
+    return this.http
+      .get<Product[]>(this.productsUrl)
+      .pipe(tap(() => console.log('In http.get pipeline')));
+  }
+
+  getProductById(id: number): Observable<Product> {
+    const productUrl = this.productsUrl + '/' + id;
+    return this.http
+      .get<Product>(productUrl)
+      .pipe(tap(() => console.log('In http.get by id pipeline')));
+  }
 }
